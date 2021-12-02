79316faa6fc9b1892172d8455e5c3bf4888a7366
Victoria Herr <victoria.herr@visionistinc.com>
2021-12-02 10:44:04 -0500

    initial commit vsherr directory

---
+++ vsherr/01/main.go
@@ -0,0 +1,6 @@
+// Day 1: Sonar Sweep
+
+package main
+
+// Count number of times a depth measurement increases from the previous measurement 
+// Answer is total number of measurements larger than the previous measurement
---
+++ vsherr/README.md
@@ -0,0 +1,8 @@
+# *Advent of Code* Solutions in Golang
+
+<center>Victoria Herr - Visionist, Inc.</center>
+<br />
+<center><img src="./golang.png" width="250" height="250"></center>
+<br />
+
+**Day 1**: Sonar Sweep
